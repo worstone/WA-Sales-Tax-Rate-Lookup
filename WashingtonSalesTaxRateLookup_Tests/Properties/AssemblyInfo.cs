0329b4bf4f01ea4a7c56d3882bebdfd9c2fb8c40
Ken Worst <ken.worst@gmail.com>
2015-01-10 23:41:33 -0800

    Fixed up AssemblyInfo

--- WashingtonSalesTaxRateLookup_Tests/Properties/AssemblyInfo.cs
+++ WashingtonSalesTaxRateLookup_Tests/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("WashingtonSalesTaxRateLookup_Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("R.C. Worst & Company Inc.")]
 [assembly: AssemblyProduct("WashingtonSalesTaxRateLookup_Tests")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyCopyright("Copyright © Ken Worst - R.C. Worst & Company Inc. 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
